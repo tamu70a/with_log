dc25cb2ea01fc4219115bde752ec7a71fde409a6
NanakoIwasaki <sibamokon@gmail.com>
2026-01-20 14:31:36 +0900

    feat: ホーム画面メモ(インライン編集)

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,15 +2,11 @@ ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
 
-module ActiveSupport
-  class ActiveSupport::TestCase
-  include Devise::Test::IntegrationHelpers
-    # Run tests in parallel with specified workers
-    parallelize(workers: :number_of_processors)
-
-    # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-    fixtures :all
-
-    # Add more helper methods to be used by all tests here...
-  end
+class ActiveSupport::TestCase
+  fixtures :all
 end
+
+class ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+end
+
