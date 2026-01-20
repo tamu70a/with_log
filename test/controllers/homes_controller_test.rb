22b508df4b3685cef92b5d427f6453419bc0c633
NanakoIwasaki <sibamokon@gmail.com>
2026-01-20 20:20:00 +0900

    feat: ホーム画面メモ(インライン編集)

--- test/controllers/homes_controller_test.rb
+++ test/controllers/homes_controller_test.rb
@@ -3,10 +3,17 @@ require "test_helper"
 class HomesControllerTest < ActionDispatch::IntegrationTest
   setup do
     @user = users(:one)
-    sign_in @user
+
+    post user_session_path, params: {
+      user: {
+        email: @user.email,
+        password: "password"
+      }
+    }
   end
+
   test "should get index" do
-    get homes_index_url
+    get root_url
     assert_response :success
   end
 end
