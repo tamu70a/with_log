e0f0efab1c31e8078f40d92f314934fd4cd8c62e
NanakoIwasaki <sibamokon@gmail.com>
2026-01-21 13:16:03 +0900

    feat: 天気API連携

--- test/controllers/homes_controller_test.rb
+++ test/controllers/homes_controller_test.rb
@@ -4,11 +4,32 @@ class HomesControllerTest < ActionDispatch::IntegrationTest
   setup do
     @user = users(:one)
     @user.create_home_memo! unless @user.home_memo
-    sign_in @user  # Deviseのヘルパーメソッドを使用
+    sign_in @user
+
+    # 元の fetch を退避
+    @original_fetch = WeatherService.method(:fetch)
+
+    # fetch を差し替え（テスト用のダミーデータ）
+    WeatherService.define_singleton_method(:fetch) do |city:|
+      {
+        "weather" => [
+          { "description" => "晴れ" }
+        ],
+        "main" => {
+          "temp" => 293.15,
+          "humidity" => 40
+        }
+      }
+    end
+  end
+
+  teardown do
+    # fetch を元に戻す
+    WeatherService.define_singleton_method(:fetch, @original_fetch)
   end
 
   test "should get index" do
-    get homes_index_url
+    get root_path
     assert_response :success
   end
 end
