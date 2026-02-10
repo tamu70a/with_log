dbb190c0f1c2ecec7801d701ec89fd5959a927da
NanakoIwasaki <sibamokon@gmail.com>
2026-02-11 01:15:39 +0900

    feat: 習慣新規登録

--- test/controllers/habits_controller_test.rb
+++ test/controllers/habits_controller_test.rb
@@ -1,8 +1,14 @@
 require "test_helper"
 
 class HabitsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @user = users(:one)
+    sign_in @user
+  end
+
   test "should get index" do
-    get habits_index_url
+    get habits_url
     assert_response :success
   end
 end
+
