95ef4306654e31f5ff23a310b48c1ebe4b853402
NanakoIwasaki <sibamokon@gmail.com>
2026-02-11 02:17:41 +0900

    feat: 習慣編集

--- app/controllers/habits_controller.rb
+++ app/controllers/habits_controller.rb
@@ -1,6 +1,6 @@
 class HabitsController < ApplicationController
   before_action :authenticate_user!
-
+  before_action :set_habit, only: [ :edit, :update, :destroy ]
   def index
     @habits = current_user.habits.order(created_at: :desc)
   end
@@ -19,9 +19,32 @@ class HabitsController < ApplicationController
     end
   end
 
+  # 編集ページ
+  def edit
+  end
+
+  # 更新処理
+  def update
+    if @habit.update(habit_params)
+      redirect_to habits_path, notice: "習慣を更新しました"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+  @habit.destroy
+  redirect_to habits_path, notice: "習慣を削除しました"
+  end
+
   private
 
   def habit_params
     params.require(:habit).permit(:title, :start_date, :end_date)
+  end
+
+  def set_habit
+    # current_user の習慣からIDで取得
+    @habit = current_user.habits.find(params[:id])
   end
 end
