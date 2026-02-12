7c0e71304431268fe866e00d5181ab8af9bc9803
NanakoIwasaki <sibamokon@gmail.com>
2026-02-13 01:06:31 +0900

    feat: メモ新規作成

--- app/models/memo.rb
+++ app/models/memo.rb
@@ -1,7 +1,7 @@
 class Memo < ApplicationRecord
   belongs_to :user
-  validates :content, presence: true
   validates :memo_date, presence: true
+  validates :content, length: { maximum: 2000 }, presence: { message: "内容を入力してください" }
 
-  default_scope { order(memo_date: :desc) }
+  default_scope { order(memo_date: :desc, created_at: :desc) }
 end
