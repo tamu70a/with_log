059c67f609e80378e0fa85fb36073f039b31ea54
NanakoIwasaki <sibamokon@gmail.com>
2026-02-14 23:20:36 +0900

    feat: 体重記録削除

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -3,24 +3,30 @@ class HomesController < ApplicationController
   before_action :authenticate_user!
 
   def index
-  res = WeatherService.fetch(city: "tokyo")
+    # 天気情報の取得
+    res = WeatherService.fetch(city: "tokyo")
 
-  @weather =
-    if res&.dig("weather", 0)
-      {
-        icon: res["weather"][0]["icon"],
-        description: res["weather"][0]["description"],
-        temp: (res["main"]["temp"] - 273.15).round(1),
-        humidity: res["main"]["humidity"]
-      }
+    @weather =
+      if res&.dig("weather", 0)
+        {
+          icon: res["weather"][0]["icon"],
+          description: res["weather"][0]["description"],
+          temp: (res["main"]["temp"] - 273.15).round(1),
+          humidity: res["main"]["humidity"]
+        }
+      end
+
+    # タスクと習慣の取得
+    @tasks = current_user.tasks.order(created_at: :desc)
+    @habits = current_user.habits.order(created_at: :desc)
+
+    # 習慣の日付補完
+    @habits.each do |habit|
+      habit.start_date ||= Date.current
+      habit.end_date ||= Date.current
     end
-  @tasks = current_user.tasks.order(created_at: :desc)
-  @habits = current_user.habits.order(created_at: :desc)
-@habits.each do |habit|
-  habit.start_date ||= Date.current
-  habit.end_date ||= Date.current
-end
 
-  @home_memo = current_user.home_memo || current_user.create_home_memo!
+    # ホームメモの取得または作成
+    @home_memo = current_user.home_memo || current_user.create_home_memo!
   end
 end
