8e3d30fc818f3bfdfbca68029c4560863700b701
NanakoIwasaki <sibamokon@gmail.com>
2026-03-16 16:08:35 +0900

    fix: タスクのバグ修正

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,13 @@
 class Task < ApplicationRecord
   belongs_to :user
   attr_accessor :editing
-  validates :title, presence: { message: "を入力してください" }, length: { maximum: 255 }
+
+  validates :title,
+    presence: { message: "を入力してください" },
+    length: { maximum: 255 },
+    unless: :editing?
+
+  def editing?
+    editing == true
+  end
 end
